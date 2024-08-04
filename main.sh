8f949ae754f9a09e89a4bbfac474cd6ab56a3f1a
aqquaa-4thewin <talinab103@gmail.com>
2024-08-04 22:23:39 +0300

    mainnnnnnn

--- main.sh
+++ main.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+declare -A person
+declare -A records
+#person=([name]="jone" [test]="LDL")
+#echo "${person[@]}"
+#records=([99]=$person)
+for line in $(cat medicalRecord.txt)
+do
+    PatientID=$(echo "$line" | cut -d":" -f1 )
+    TestName=$(echo "$line" | cut -d":" -f2 | cut -d"," -f1 )
+    Testdate=$(echo "$line" | cut -d":" -f2 | cut -d"," -f2 )
+    PatientResult=$(echo "$line" | cut -d":" -f2 | cut -d"," -f3 )
+    Testunit=$(echo "$line" | cut -d":" -f2 | cut -d"," -f4 )
+    Teststatus=$(echo "$line" | cut -d":" -f2 | cut -d"," -f5 )
+    person=([name]=$TestName [date]=$Testdate [resutl]=$PatientResult [unit]=$Testunit [status]=$Teststatus)
+    #echo "${person[name]} ${person[date]}"
+    records=(["$PatientID"]="${person[@]}")
+    
+    
+done 
+for key in "${!records[@]}"
+do 
+echo "${records[$key]}"
+done
