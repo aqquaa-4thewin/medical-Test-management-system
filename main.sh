ac7a0a3a0dbbf7af9ca7592ac19369fc640f7bd5
aqquaa-4thewin <talinab103@gmail.com>
2024-08-11 00:51:18 +0300

    finallll

--- main.sh
+++ main.sh
@@ -39,7 +39,7 @@ declare -A normal_ranges
 # Read the test ranges from the file and populate the associative array
 while IFS= read -r line || [ -n "$line" ]; do
     # Extract test name
-    test_name=$(echo "$line" | grep -oP '(?<=\().*(?=\))')
+    test_name=$(echo "$line" | grep -oP '(?<=\().*(?=\))' |  tr '[:upper:]' '[:lower:]')
     
     # Extract range
     range=$(echo "$line" | grep -oP '(?<=Range: ).*(?=; Unit)')
@@ -52,7 +52,7 @@ while IFS= read -r line || [ -n "$line" ]; do
     fi
 
     if [ -z "$upper_limit" ]; then
-        upper_limit=9999  # Or any large value 
+        upper_limit=9999  # large value 
     fi
 
     normal_ranges["$test_name"]="$lower_limit $upper_limit"
@@ -78,29 +78,73 @@ search_menu(){
     printf "\n 1-Retrieve all patient tests\n 2-Retrieve all up normal patient tests\n 3-Retrieve all patient tests in a given specific period\n 4-Retrieve all patient tests based on test status"
 }
 
-Add(){ # Handeling !!!!!
+Add(){ # Add a medical test
 
-    # while [ 0 -eq 0 ]
-    # do 
+    while [ 0 -eq 0 ]
+    do 
     printf "\nEnter Patient ID: "
     read Id
-    # if [ echo $Id | wc -c -eq 7 ]
-    # then
-    #     break
-    # fi
-    # printf("\n Invalid ID")
-    #done
+    check_Id $Id
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
+
+    while [ 0 -eq 0 ]
+    do 
     printf "\nEnter Test Name: "
     read Name
+    check_name $Name
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
+    
+    while [ 0 -eq 0 ]
+    do 
     printf "\nEnter Date: "
     read Date
-    printf "\nEnter result with unit: "
+    check_date $Date
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
+    
+    while [ 0 -eq 0 ]
+    do 
+    printf "\nEnter result: "
     read Result
-    printf "\nEnter status: "
-    read Status
+    check_result $Result
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
 
-    echo -e "\n$Id:$Name,$Date,$Result,$Status" >> medicalRecord.txt
-    printf "record has been added successfully"
+    while [ 0 -eq 0 ]
+    do 
+    printf "\nEnter status ( Pending ,  Completed ,  Reviewed ): "
+    read Status
+    check_status $Status
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
+
+    unit=$(grep -i "$Name" medicalTest.txt | cut -d":" -f4 | xargs) # -i greps regardless of upper or lower cases
+
+    #echo -e "\n$Id:$Name,$Date,$Result,$unit,$Status" >> medicalRecord.txt
+    printf "%s" "$(echo -e "\n$Id:$Name,$Date,$Result,$unit,$Status")" >> medicalRecord.txt # to remove new line with copy
+    printf "  record has been added successfully"
 
 }
 
@@ -115,9 +159,10 @@ Avg(){
             sum=$(echo "$sum + $PatientResult" | bc)
             count=$(($count+1))
         done < temp.txt
+        unit=$(grep -i "$symbol" medicalTest.txt | cut -d":" -f4 | xargs)
         if [ "$count" -gt 0 ]; then
             avg=$(echo "scale=2; $sum / $count" | bc)  #  bc for floating-point division, scale for decimal fractions
-            echo -e "\nThe average for $symbol is $avg"
+            echo -e "\nThe average for $symbol is $avg $unit"
         else
             echo -e "\nNo records found for $symbol, cannot compute average."
         fi
@@ -125,18 +170,45 @@ Avg(){
 }
 
 update(){
-    printf "\n enter patient ID: "
-    read id
+    while [ 0 -eq 0 ]
+    do 
+        printf "\n enter patient ID: "
+        read id
+        check_Id $id
+        status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
+
     grep  $id medicalRecord.txt > temp.txt
     printf "\n available tests are:\n"
     cat -n temp.txt
-    printf "\n choose a test: "
-    read choice
+    while [ 0 -eq 0 ]
+    do 
+        printf "\n choose a test: "
+        read choice
+        if [ $choice -le "$(cat temp.txt | wc -l)" ] &&  [ $choice -gt 0 ]
+        then
+        break
+        fi
+        printf "\n Invalid Option"
+    done
 
     record=$(sed -n "${choice}p" temp.txt)
     echo "$record" > temp.txt
-    printf "\n Enter new result: "
+    while [ 0 -eq 0 ]
+    do 
+    printf "\nEnter new result: "
     read newresult
+    check_result $newresult
+    status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
 
     result=$(echo "$record" | cut -d":" -f2 | cut -d"," -f3 | xargs)
     newrecord=$(sed "s/$result/$newresult/g" temp.txt)
@@ -146,27 +218,43 @@ update(){
 }
 
 search_id(){
-    printf "\n enter patient ID: "
-    read id
+    while [ 0 -eq 0 ]
+    do 
+        printf "\n enter patient ID: "
+        read id
+        check_Id $id
+        status=$?
+        if [ $status -eq 0 ]
+        then 
+            break
+        fi 
+    done
     search_menu
-    printf "\n choose an option:  "
-    read  choice
-    case $choice in
-        "1")
-        print_by_ID "$id"
-        ;;
-        "2")
-        Abnormal_ID "$id"
-        ;;
-        "3")
-        print_in_period "$id"
-        ;;
-        "4")
-        print_by_status "$id"
-        ;;
-        *)
-        echo " invalid option !!";;
-    esac
+    while [ 0 -eq 0 ]
+    do 
+        printf "\n choose an option:  "
+        read  choice
+        case $choice in
+            "1")
+            print_by_ID "$id"
+            break
+            ;;
+            "2")
+            Abnormal_ID "$id"
+            break
+            ;;
+            "3")
+            print_in_period "$id"
+            break
+            ;;
+            "4")
+            print_by_status "$id"
+            break
+            ;;
+            *)
+            echo " invalid option !!";;
+        esac
+    done
 
 }
 print_by_ID(){
@@ -181,8 +269,22 @@ print_by_ID(){
 print_by_status(){
     id=$1
     grep  $id medicalRecord.txt > temp.txt
-    printf '\n Enter status: (Pending", Completed, Reviewed)'
-    read status
+    while [ 0 -eq 0 ]
+    do 
+        printf "\nEnter status ( Pending ,  Completed ,  Reviewed ): "
+        read status
+        check_status $status
+        stat=$?
+            if [ $stat -eq 0 ]
+            then 
+                break
+            fi 
+    done
+
+    if [ ! -s temp.txt ] ; then # check if file is empty
+        echo -e "\n no records for patient $id with status $status"
+        return
+    fi
     grep "$status" temp.txt
     
 
@@ -191,16 +293,46 @@ print_by_status(){
 print_in_period(){
     id=$1
     grep  $id medicalRecord.txt > temp.txt
-    printf "\n Enter first Date: "
-    read DateFrom
-    YFrom=$(echo "$DateFrom" | cut -d"-" -f1)
-    MFrom=$(echo "$DateFrom" | cut -d"-" -f2)
+    while [ 0 -eq 0 ]
+    do 
+        while [ 0 -eq 0 ]
+        do 
+            printf "\n Enter first Date: "
+            read DateFrom
+            check_date $DateFrom
+            stat=$?
+                if [ $stat -eq 0 ]
+                then 
+                    break
+                fi 
+        done
 
-    printf "\n Enter second Date: "
-    read DateTo
-    YTo=$(echo "$DateTo" | cut -d"-" -f1)
-    MTo=$(echo "$DateTo" | cut -d"-" -f2)
-    printf "\n"
+        YFrom=$(echo "$DateFrom" | cut -d"-" -f1)
+        MFrom=$(echo "$DateFrom" | cut -d"-" -f2)
+
+        while [ 0 -eq 0 ]
+        do 
+            printf "\n Enter second Date: "
+            read DateTo
+            stat=$?
+                if [ $stat -eq 0 ]
+                then 
+                    break
+                fi 
+        done
+
+        YTo=$(echo "$DateTo" | cut -d"-" -f1)
+        MTo=$(echo "$DateTo" | cut -d"-" -f2)
+        printf "\n"
+
+        if [ "$YFrom" -gt "$YTo" ] || { [ "$YFrom" -eq "$YTo" ] && [ "$MFrom" -gt "$MTo" ]; }; then
+            printf "\nEnter 'From' date that is earlier than 'To' date\n"
+        else
+            break
+        fi
+    done
+
+
 
 
     while IFS= read -r line; do
@@ -230,7 +362,7 @@ Abnormal_ID(){
     
     echo "Abnormal tests for patient ID $id:"
     while IFS= read -r record; do # loop over record of patient
-        test_name=$(echo "$record" | cut -d',' -f1 | cut -d':' -f2 | xargs) # test name
+        test_name=$(echo "$record" | cut -d',' -f1 | cut -d':' -f2 |  tr '[:upper:]' '[:lower:]' | xargs) # test name
         test_result=$(echo "$record" | cut -d',' -f3 | xargs) # test result
     
         range="${normal_ranges[$test_name]}" # ranges are taken from associative array
@@ -247,9 +379,20 @@ Abnormal_ID(){
 }
 
 search_Abnormal_by_testname(){
-    printf "\n enter Test name: "
-    read name
-    records=$(grep "$name" medicalRecord.txt)
+
+    while [ 0 -eq 0 ]
+    do 
+        printf "\nEnter Test Name: "
+        read name
+        check_name $name
+        status=$?
+            if [ $status -eq 0 ]
+            then 
+                break
+            fi 
+    done
+
+    records=$(grep -i "$name" medicalRecord.txt)
 
     if [ -z "$records" ]; then # handling if no records found 
         echo "No records found for Test name $id."
@@ -259,7 +402,7 @@ search_Abnormal_by_testname(){
     echo  " Abnormal tests for $name are:"
 
     while IFS= read -r record; do # loop over record of patient
-        test_name=$(echo "$record" | cut -d',' -f1 | cut -d':' -f2 | xargs) # test name
+        test_name=$(echo "$record" | cut -d',' -f1 | cut -d':' -f2 | tr '[:upper:]' '[:lower:]' | xargs) # test name
         test_result=$(echo "$record" | cut -d',' -f3 | xargs) # test result
     
         range="${normal_ranges[$test_name]}" # ranges are taken from associative array
@@ -272,6 +415,104 @@ search_Abnormal_by_testname(){
             echo "$record"
         fi
     done <<< "$records"
+
+}
+
+
+check_Id(){
+    id=$(echo "$1" | xargs)
+    id=$(echo "$id" | tr -d "\n")
+    digits=$(echo "$id" | wc -c)
+  
+    if [ $digits -ne 8 ] # don't forget null notation '\0'
+    then
+        printf "\n Invalid ID"
+        return 1
+    fi
+
+    if [[ $id =~ ^[0-9]+$ ]]; then
+        return 0
+    else
+        printf "\n Invalid ID"
+        return 1
+    fi
+}
+
+check_name(){
+    name=$(echo "$1" | tr '[:upper:]' '[:lower:]') # from uppercase to lower case
+    flag=0
+    for key in "${!normal_ranges[@]}" ;do
+        testname=$(echo "$key" | tr '[:upper:]' '[:lower:]')
+        if [ "$name" = "$testname" ]
+        then
+            flag=1
+        fi
+    done
+    if [ $flag -eq 0 ]
+    then
+    printf "\n Invalid Test name"
+    return 1
+    fi
+    return 0
+}
+
+check_date(){
+    date=$(echo "$1" | xargs)
+    date=$(echo "$date" | tr -d "\n")
+    year=$(echo "$date" | cut -d '-' -f1)
+    month=$(echo "$date" | cut -d '-' -f2)
+
+    if [ "$(echo "$date" | wc -c)" -ne 8 ]
+    then
+        printf "\nInvalid format ( YYYY-MM )" # YYYY-MM
+        return 1
+    fi
+    
+    if [ -z "$month" ] || [[ ! $month =~ ^[0-9]+$ ]] || [[ ! $year =~ ^[0-9]+$ ]]; then
+        printf "\nInvalid format "
+        return 1
+    fi
+    #!!!!!!!!! check year 4 chars , month 2 chars
+    if [ "$(echo "$year" | wc -c)" -ne 5 ] || [ "$(echo "$month" | wc -c)" -ne 3 ]
+    then
+        printf "\nInvalid format"
+        return 1
+    fi
+    if [ "$year" -ge 2025 ] || [ "$year" -le 0 ] || [ "$month" -gt 12 ] || [ "$month" -le 0 ]
+    then
+        printf "\nInvalid Date"
+        return 1
+    fi
+
+    return 0
+
+}
+
+check_result(){
+    result=$1
+
+    if [[ "$result" =~ ^[0-9]+$ ]]; then # check for an integer that is positive
+        return 0
+    fi
+
+    if [[ "$result" =~ ^[0-9]*\.[0-9]+$ ]] || [[ "$result" =~ ^[0-9]+\.[0-9]*$ ]]; then # check for a float with decimal point that is not negative
+        return 0
+    else
+        printf "\n Invalid result "
+        return 1
+    fi
+
+}
+
+check_status(){
+    status=$(echo "$1" | tr '[:upper:]' '[:lower:]' | xargs)
+    if [ "$status" = "completed" ] || [ "$status" = "pending" ] || [ "$status" = "reviwed" ]
+    then    
+        return 0
+    else 
+        printf "\n Invalid status "
+        return 1
+    fi
 
 }
 
